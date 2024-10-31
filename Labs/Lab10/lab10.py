b1357e6dcd99dcad5318972b78d776910d66473c
Liz2107 <elcu5473@colorado.edu>
2024-10-31 14:37:02 -0600

    lab10 work

--- Labs/Lab10/lab10.py
+++ Labs/Lab10/lab10.py
@@ -25,7 +25,7 @@ def driver():
     # weight function
     w = lambda x: 1.
     # order of approximation
-    n = 2
+    n = 10
     # Number of points you want to sample in [a,b]
     N = 1000
     xeval = np.linspace(a,b,N+1)
@@ -41,29 +41,30 @@ def driver():
     plt.plot(xeval,pval,'bs--',label= 'Expansion')
     plt.legend()
     plt.show()
-    err = abs(pval-fex)
-    plt.semilogy(xeval,err_l,'ro--',label='error')
-    plt.legend()
-    plt.show()
+    # err = abs(pval-fex)
+    # # plt.semilogy(xeval,err_l,'ro--',label='error')
+    # plt.legend()
+    # plt.show()
 
 def eval_legendre_expansion(f,a,b,w,n,x):
     # This subroutine evaluates the Legendre expansion
     # Evaluate all the Legendre polynomials at x that are needed
     # by calling your code from prelab
     p = eval_legendre(n, x)
+    # p = [1, x, (3*x-1)/2]
     # initialize the sum to 0
     pval = 0.0
     for j in range(0,n+1):
         # make a function handle for evaluating phi_j(x)
-        phi_j = lambda x: eval_legendre(j, x)
+        phi_j = lambda x: eval_legendre(j, x)[j]
         # make a function handle for evaluating phi_j^2(x)*w(x)
         phi_j_sq = lambda x: phi_j(x)**2 * w(x)
         # use the quad function from scipy to evaluate normalizations
-        norm_fac,err = ...
+        norm_fac,err = quad(phi_j_sq, a, b)
         # make a function handle for phi_j(x)*f(x)*w(x)/norm_fac
         func_j = lambda x: phi_j(x) * f(x) * w(x) / norm_fac
         # use the quad function from scipy to evaluate coeffs
-        aj,err = ...
+        aj,err = quad(func_j, a, b)
         # accumulate into pval
         pval = pval+aj*p[j]
     return pval
